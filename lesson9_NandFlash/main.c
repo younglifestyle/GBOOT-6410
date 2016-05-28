bcba16c4c08f42bc251dd440debcf993f32f24b9
“younglifestyle” <718800822@qq.com>
2016-05-28 15:11:36 +0800

    完成了NF的写、读和擦除操作，在main函数中测试了写和读，没有问题，这里需要注意NFDATA寄存器发送的数据，八位有效，其他位为垃圾数据

--- lesson9_NandFlash/main.c
+++ lesson9_NandFlash/main.c
@@ -3,12 +3,27 @@ int gboot_main()
 #ifdef MMU_ON
 	mmu_start()
 #endif
-
+	unsigned char buf[2048];
+	
 	led_init();
 	key_init();
 	init_irq();
-	
 	led_on();
+	NF_Page_Erase(5*128+1);
+	
+	buf[0] = 100;
+	NF_Page_Write(5*128+1, buf);
+	
+	buf[0] = 10;
+	
+	NF_Page_Read(5*128+1, buf);
+	if(buf[0] == 100)
+		led_off();
+	else if(buf[0] == 10)
+		led_on();
+	else
+		;
+	
 	while(1);
 		
 	return 0;
--- lesson9_NandFlash/nand.c
+++ lesson9_NandFlash/nand.c
@@ -5,6 +5,10 @@
 #define NFDATA (*(volatile unsigned char *)0x70200010)
 #define NFSTAT (*(volatile unsigned char *)0x70200028)
 
+#define TACLS  0x2
+#define TWRPH0 0xf
+#define TWRPH1 0x7
+
 void select_chip()
 {
 	NFCONT &= ~(1 << 1);
@@ -55,16 +59,11 @@ void reset()
 
 void nand_init()
 {
-	    /* 设置时间参数 */
-#define TACLS  7
-#define TWRPH0 7
-#define TWRPH1 7
-    
-    NFCONF &= ~((7<<12)|(7<<8)|(7<<4));
-    NFCONF |= (TACLS<<12)|(TWRPH0<<8)|(TWRPH1<<4);
-    
-    /* 使能 nandflash controller*/
-    NFCONT = 1 | (1<<1);
+	/* 设置时间参数 */
+	NFCONF |= ( (TACLS<<12)|(TWRPH0<<8)|(TWRPH1<<4) );
+	
+	/* 使能 nandflash controller*/
+	NFCONT = 1 | (1<<1);
 	
 	// Reset
 	reset();
@@ -119,4 +118,85 @@ void copy_2ddr(unsigned long nand_start, unsigned char *buff, int size)
 		size -= 2048;
 		i++;
 	}
+}
+
+
+void NF_Page_Write(unsigned long addr, unsigned char *buff)
+{
+	int i;
+	int ret;
+	
+	// 选中NF
+	select_chip();
+	
+	// 清除RnB
+	clear_RnB();
+	
+	// 发送0x80命令
+	send_cmd(0x80);
+	
+	// 发送2个列地址
+	send_addr(0x00);
+	send_addr(0x00);
+	
+	// 发送3个行地址
+	send_addr(addr & 0xFF);
+	send_addr((addr >> 8) & (0xFF));
+	send_addr((addr >> 16) & (0xFF));
+	
+	// 写入数据
+	for(i=0; i < 1024*2; i++)
+	{
+		NFDATA = *buff++;
+	}
+	
+	// 发送0x10命令
+	send_cmd(0x10);
+	
+	// 等待RnB信号
+	wait_RnB();
+	
+	// 发送0x70命令
+	send_cmd(0x70);
+	
+	// 读取写入状态
+	ret = NFDATA;
+	
+	//取消选中
+	delete_chip();
+}
+
+void NF_Page_Erase(unsigned long addr)
+{
+	int i;
+	int ret;
+	
+	// 选中NF
+	select_chip();
+	
+	// 清除RnB
+	clear_RnB();
+	
+	// 发送0x60命令
+	send_cmd(0x60);
+	
+	// 发送3个行地址
+	send_addr(addr & 0xFF);
+	send_addr((addr >> 8) & (0xFF));
+	send_addr((addr >> 16) & (0xFF));
+	
+	// 发送0xD0命令
+	send_cmd(0xD0);
+	
+	// 等待RnB信号
+	wait_RnB();
+	
+	// 发送0x70命令
+	send_cmd(0x70);
+	
+	// 读取写入状态
+	ret = NFDATA;
+	
+	//取消选中
+	delete_chip();
 }
